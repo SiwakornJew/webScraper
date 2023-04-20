e27f0581e911792a03285c18ffb1803992960692
Siwakorn Noisundod <69206848+SiwakornJew@users.noreply.github.com>
2023-04-21 00:16:32 +0700

    put

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 
 from fastapi import FastAPI
 from getTemple import getTem
-
+from fastapi.responses import FileResponse
 app = FastAPI()
 
 
 @app.get("/temple/{provide_name}")
 async def getTemple(provide_name):
     li_match = getTem(provide_name)
-
     return li_match
+
+
+@app.get("/temple/list_of_temple-csv")
+async def download_csv():
+    file_path = "list_temple.csv"
+    return FileResponse(file_path, media_type="text/csv", filename="list_temple.csv")
